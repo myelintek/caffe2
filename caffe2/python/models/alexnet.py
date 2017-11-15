108cbb67bc0f56d011bbbfe254f03e6eeaccac96
SimonChuang <simon.s.chuang@gmail.com>
2017-11-15 09:28:30 +0000

    Add alexnet py & ipynb

--- caffe2/python/models/alexnet.py
+++ caffe2/python/models/alexnet.py
@@ -22,88 +22,111 @@ def create_alexnet(
     conv1_stride=2,
     final_avg_kernel=7,
 ):
+    # conv1 + maxpool
     conv1 = brew.conv(
         model,
         data,
         "conv1",
         num_input_channels,
         96,
-        11, ('XavierFill', {}), ('ConstantFill', {}),
+        kernel=11,
+        weight_init=('MSRAFill', {}),
         stride=4,
         pad=2
     )
-    relu1 = brew.relu(model, conv1, "relu1")
-    norm1 = brew.lrn(model, relu1, "norm1", size=5, alpha=0.0001, beta=0.75)
+    brew.relu(model, conv1, "conv1")
+    norm1 = brew.lrn(model, conv1, "norm1", size=5, alpha=0.0001, beta=0.75)
     pool1 = brew.max_pool(model, norm1, "pool1", kernel=3, stride=2)
+
     conv2 = brew.group_conv(
         model,
         pool1,
         "conv2",
         96,
         256,
-        5,
-        ('XavierFill', {}),
-        ('ConstantFill', {}),
+        kernel=5,
+        weight_init=('MSRAFill', {}),
         group=2,
         stride=1,
         pad=2
     )
-    relu2 = brew.relu(model, conv2, "relu2")
-    norm2 = brew.lrn(model, relu2, "norm2", size=5, alpha=0.0001, beta=0.75)
+    brew.relu(model, conv2, "conv2")
+    norm2 = brew.lrn(model, conv2, "norm2", size=5, alpha=0.0001, beta=0.75)
     pool2 = brew.max_pool(model, norm2, "pool2", kernel=3, stride=2)
+
     conv3 = brew.conv(
         model,
         pool2,
         "conv3",
         256,
         384,
-        3,
-        ('XavierFill', {}),
-        ('ConstantFill', {}),
+        kernel=3,
+        weight_init=('MSRAFill', {}),
         pad=1
     )
-    relu3 = brew.relu(model, conv3, "relu3")
+    brew.relu(model, conv3, "conv3")
     conv4 = brew.group_conv(
         model,
-        relu3,
+        conv3,
         "conv4",
         384,
         384,
-        3,
-        ('XavierFill', {}),
-        ('ConstantFill', {}),
+        kernel=3,
+        weight_init=('MSRAFill', {}),
         group=2,
         pad=1
     )
-    relu4 = brew.relu(model, conv4, "relu4")
+    brew.relu(model, conv4, "conv4")
     conv5 = brew.group_conv(
         model,
-        relu4,
+        conv4,
         "conv5",
         384,
         256,
-        3,
-        ('XavierFill', {}),
-        ('ConstantFill', {}),
+        kernel=3,
+        weight_init=('MSRAFill', {}),
         group=2,
         pad=1
     )
-    relu5 = brew.relu(model, conv5, "relu5")
-    pool5 = brew.max_pool(model, relu5, "pool5", kernel=3, stride=2)
+    brew.relu(model, conv5, "conv5")
+    pool5 = brew.max_pool(model, conv5, "pool5", kernel=3, stride=2)
     fc6 = brew.fc(
         model,
-        pool5, "fc6", 256 * 6 * 6, 4096, ('XavierFill', {}),
-        ('ConstantFill', {})
+        pool5,
+        "fc6",
+        256 * 6 * 6,
+        4096
     )
-    relu6 = brew.relu(model, fc6, "fc6")
-    drop6 = brew.dropout(model, relu6, "drop6", use_cudnn=True)
+    relu6 = brew.relu(model, fc6, "relu6")
+    drop6 = brew.dropout(model, relu6, "drop6", use_cudnn=True, ratio=0.5, is_test=is_test)
     fc7 = brew.fc(
-        model, drop6, "fc7", 4096, 4096, ('XavierFill', {}), ('ConstantFill', {})
+        model,
+        drop6,
+        "fc7",
+        4096,
+        4096
     )
-    relu7 = brew.relu(model, fc7, "fc7")
-    drop7 = brew.dropout(model, relu7, "drop7", use_cudnn=True)
-    fc8 = brew.fc(
-        model, drop7, "fc8", 4096, 1000, ('XavierFill', {}), ('ConstantFill', {})
+    brew.relu(model, fc7, "fc7")
+    drop7 = brew.dropout(model, fc7, "drop7", use_cudnn=True, ratio=0.5, is_test=is_test)
+    last_out = brew.fc(
+        model,
+        drop7,
+        'last_out_L{}'.format(num_labels),
+        4096,
+        num_labels
     )
-    softmax = brew.softmax(model, fc8, "softmax")
-    return softmax
+
+    if no_loss:
+        return last_out
+
+    # If we create model for training, use softmax-with-loss
+    if (label is not None):
+        (softmax, loss) = model.SoftmaxWithLoss(
+            [last_out, label],
+            ["softmax", "loss"],
+        )
+
+        return (softmax, loss)
+    else:
+        # For inference, we just return softmax
+        return brew.softmax(model, last_out, "softmax")
